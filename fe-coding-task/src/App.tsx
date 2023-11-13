2b16feac035b1ec8e41fa694f9f761e555ad9a09
Dominik Pietrzyca <defres15@gmail.com>
2023-11-13 19:47:25 +0100

    initial commit

---
+++ fe-coding-task/src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+
+// - Ekran 1 wersja statyczna
+// - Wyciągnięcie z API - zakres kwartałów i typ budynku
+// - Ograniczenie podawania wartości kwartałów wcześniej niż 2009K1
+// - parametry powinny być zachowane między sesjami przeglądarki
+// - Te parametry muszą być odzwierciedlone w adresie URL
+
+// - wykres liniowy/słupkowy
+// - Możliwość zapisywania statystyk do dalszego wykorzystania
+// - Możliwość dodawania pojedynczych komentarzy do prezentowanych statystyk
+// - Możliwość edytowania pojedynczych komentarzy do prezentowanych statystyk
+// - Komentarz może być przechowywany w pamięci lokalnej (Local Storage)
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Test</header>
+    </div>
+  );
+}
+
+export default App;
